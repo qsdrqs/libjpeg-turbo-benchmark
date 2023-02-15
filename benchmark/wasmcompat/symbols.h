c727017ffa2658fbad761fea968aeca56c596283
qsdrqs <qsdrqs@gmail.com>
2023-02-14 22:15:08 -0800

    add header for rlbox-rust

---
+++ benchmark/wasmcompat/symbols.h
@@ -0,0 +1,22 @@
+#ifndef _SYMBOLS_H
+#define _SYMBOLS_H
+
+typedef __SIZE_TYPE__ size_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __FILE_defined
+#define __FILE_defined 1
+
+struct _IO_FILE;
+
+typedef struct _IO_FILE FILE;
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
